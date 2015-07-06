670629bf4e2f248c0dfa8555f25ecfe7d86e3b92
David Kenny <dmkinteractive@gmail.com>
2015-07-06 16:14:12 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!-- INCLUDE HEADER -->
+<?php
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/header.html";
+   include_once($path);
+?>
+
+
+<!-- BEGIN PAGE CONTENT -->
+<div>
+    <h1 class="animated fadeIn">graftwork</h1>
+	<p class="animated fadeIn">
+		Design by <a href="http://davidkenny.me" target="_blank">David Kenny</a>
+	</p>
+</div>
+<!-- END PAGE CONTENT -->
+
+
+<!-- INCLUDE FOOTER -->
+<?php
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/footer.html";
+   include_once($path);
+?>
